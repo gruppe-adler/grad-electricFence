261df14501a5455f4f5afb99e6bbf919338724e5
simon.schmidt <simon.schmidt@idealo.de>
2018-11-08 22:09:13 +0100

    added fnc

--- cfgFunctions.hpp
+++ cfgFunctions.hpp
@@ -6,18 +6,22 @@ class grad_electricFence {
     class electricFence {
         file = MODULES_DIRECTORY\grad-electricFence\functions;
 
-        class canLockpick {};
-        class canPeeOn {};
-        class createShockSparks {};
-        class createSmoke {};
-        class initFence {};
-        class initGate {};
-        class lockDoors {};
-        class onActionLockpick {};
-        class onActionPee {};
-        class peeEffect {};
-        class shockUnit {};
-        class sparksEffectClient {};
-        class sparksEffectServer {};
+        class addFencePart;
+        class addGenerator;
+        class canLockpick;
+        class canPeeOn;
+        class createShockSparks;
+        class createSmoke;
+        class generateSerial;
+        class initFence;
+        class initGate;
+        class lockDoors;
+        class onActionLockpick;
+        class onActionPee;
+        class peeEffect;
+        class shockUnit;
+        class sparksEffectClient;
+        class sparksEffectServer;
+        class sparksGetPos;
     };
 };
